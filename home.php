c01353f970f424cee503e63ed99b721a78ebf318
nemopotatoes <jesgjx@gmail.com>
2022-10-21 20:30:50 +0800

    moved header navigation to nav_header.php

--- home.php
+++ home.php
@@ -8,26 +8,8 @@
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
 </head>
 <body>
-	<header class="header">
-		<nav class="nav">
-			<a href="home.php" class="logo">Logo</a>
 
-			<nav class="navbar">
-				<a href="home.php">Home</a>
-				<a href="about.php">About</a>
-				<a href="menu.php">Menu</a>
-				<a href="contact.php">Contact Us</a>
-				<a href="orders.php">Orders</a>
-			</nav>
-
-			<div class="icons">
-				<a href="search.php"><i class="fas fa-search"></i></a>
-				<a href="cart.php"><i class="fas fa-shopping-cart"></i></a>
-				<div id="user-btn" class="fas fa-user"></div>
-				<div id="menu-btn" class="fas fa-bars"></div>
-			</div>
-		</nav>
-	</header>
+<?php include 'components/nav_header.php'; ?>
 
 	<div class="carousell-container">
 
