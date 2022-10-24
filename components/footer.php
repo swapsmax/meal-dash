59bc016fb4b15f7c8b5540d2ed459b77e1ac60af
nemopotatoes <jesgjx@gmail.com>
2022-10-24 23:33:43 +0800

    base codes for footer

---
+++ components/footer.php
@@ -0,0 +1,31 @@
+<div class="footer">
+
+    <div class="container">
+        <a class="box">
+            <img src="images/email-icon.png" alt="">
+            <h3>Email Us!</h3>
+            <p>MealDash.com</p>
+        </a>
+
+        <a class="box">
+            <img src="images/clock-icon.png" alt="">
+            <h3>Opening Hours</h3>
+            <p>8.00am to 10.00pm</p>
+        </a>
+
+        <a class="box">
+            <img src="images/map-icon.png" alt="">
+            <h3>Located At:</h3>
+            <p>NTU, Singapore</p>
+        </a>
+
+        <a class="box">
+            <img src="images/phone-icon.png" alt="">
+            <h3>Contact Us</h3>
+            <p>123-456-7890</p>
+        </a>
+
+        <br>
+        <div class="copyright">Copyright &copy; 2022 <a href="">Meal Dash</a></div>
+    </div>
+</div>
