07ca784592ccaacc5eeb1eb204192d067c6e02b5
Swapnil <46750394+swapsmax@users.noreply.github.com>
2022-10-25 14:53:18 +0800

    Updated nav_header.php

--- components/nav_header.php
+++ components/nav_header.php
@@ -12,7 +12,6 @@
 			<a href="search.php"><i class="fas fa-search"></i></a>
 			<a href="cart.php"><i class="fas fa-shopping-cart"></i></a>
 			<div id="user-btn" class="fas fa-user"></div>
-			<div id="menu-btn" class="fas fa-bars"></div>
 		</div>
 	</nav>
 </header>
