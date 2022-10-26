854418c306a5ab1a9d76bb9f3aa30bd7cf2ac1a8
Swapnil <46750394+swapsmax@users.noreply.github.com>
2022-10-26 13:28:05 +0800

    Updated login.php

---
+++ login.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Login</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/stylesheet.css">
+
+</head>
+<body>
+   
+<!-- header section starts  -->
+   <?php include 'components/nav_header.php'; ?>
+<!-- header section ends -->
+
+<div class="form-container">
+
+   <form action="" method="post">
+      <h3>login now</h3>
+      <input type="email" name="email" required placeholder="enter your email" class="box" maxlength="50" oninput="this.value = this.value.replace(/\s/g, '')">
+      <input type="password" name="pass" required placeholder="enter your password" class="box" maxlength="50" oninput="this.value = this.value.replace(/\s/g, '')">
+      <input type="submit" value="login now" name="submit" class="btn">
+      <p>don't have an account? <a href="register.php">register now</a></p>
+   </form>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+<?php include 'components/footer.php'; ?> 
+
+
+
+
+
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
