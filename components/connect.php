dc11c44c3507f13133a215b2400802d122a4e457
nemopotatoes <jesgjx@gmail.com>
2022-10-26 15:13:49 +0800

    updated connect.php - database connection

--- components/connect.php
+++ components/connect.php
@@ -1,9 +1,5 @@
 <?php
 
-$db_name = 'mysql:host=localhost;dbname=food_db';
-$user_name = 'root';
-$user_password = '';
-
-$conn = new PDO($db_name, $user_name, $user_password);
+$conn = mysqli_connect('localhost', 'root', '', 'mealdash');
 
 ?>
