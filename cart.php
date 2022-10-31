49c8ba8fdec07ce238d451984097e5e9325e3185
nemopotatoes <jesgjx@gmail.com>
2022-10-31 18:50:52 +0800

    cart.php base code

---
+++ cart.php
@@ -0,0 +1,118 @@
+<!-- include php database connection here -->
+<?php
+
+include 'components/connect.php';
+
+session_start();
+
+// if(isset($_SESSION['user_id'])){
+//    $user_id = $_SESSION['user_id'];
+// }else{
+//    $user_id = '';
+//    header('location:home.php');
+// };
+
+if(isset($_POST['delete'])){
+   $cart_id = $_POST['cart_id'];
+   $delete_cart_item = $conn->prepare("DELETE FROM `cart` WHERE id = ?");
+   $delete_cart_item->execute([$cart_id]);
+   $message[] = 'cart item deleted!';
+}
+
+if(isset($_POST['delete_all'])){
+   $delete_cart_item = $conn->prepare("DELETE FROM `cart` WHERE user_id = ?");
+   $delete_cart_item->execute([$user_id]);
+   header('location:cart.php');
+   $message[] = 'deleted all from cart!';
+}
+
+if(isset($_POST['update_qty'])){
+   $cart_id = $_POST['cart_id'];
+   $qty = $_POST['qty'];
+   $qty = filter_var($qty, FILTER_SANITIZE_STRING);
+   $update_qty = $conn->prepare("UPDATE `cart` SET quantity = ? WHERE id = ?");
+   $update_qty->execute([$qty, $cart_id]);
+   $message[] = 'cart quantity updated';
+}
+
+$grand_total = 0;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Your Cart</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="css/stylesheet.css">
+	<!-- font awesome -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+</head>
+
+<body>
+    <?php include 'components/nav_header.php'; ?>
+
+    <div class="cart-heading">
+        <h3>My Shopping Cart</h3>
+    </div>
+
+    <div class="products">
+
+        <div class="container">
+
+            <?php
+            // code is altered for styling, 
+            // needs to be corrected for when user sign in can be done
+            // to view cart of another user, manually change user_id = on line 70
+                $grand_total = 0;
+                $select_cart = $conn->prepare("SELECT * FROM `cart` WHERE user_id = 1");
+                $select_cart->execute();
+                if($select_cart->rowCount() > 0){
+                    while($fetch_cart = $select_cart->fetch(PDO::FETCH_ASSOC)){
+            ?>
+            <form action="" method="post" class="box">
+                <input type="hidden" name="cart_id" value="<?= $fetch_cart['id']; ?>">
+                <a href="quick_view.php?pid=<?= $fetch_cart['pid']; ?>" class="fas fa-eye"></a>
+                <button type="submit" class="fas fa-times" name="delete" onclick="return confirm('delete this item?');"></button>
+                <img src="uploaded_img/<?= $fetch_cart['image']; ?>" alt="">
+                <div class="name"><?= $fetch_cart['name']; ?></div>
+                <div class="flex">
+                    <div class="price"><span>$</span><?= $fetch_cart['price']; ?></div>
+                    <input type="number" name="qty" class="qty" min="1" max="99" value="<?= $fetch_cart['quantity']; ?>" maxlength="2">
+                    <button type="submit" class="fas fa-edit" name="update_qty"></button>
+                </div>
+                <div class="sub-total"> sub total : <span>$<?= $sub_total = ($fetch_cart['price'] * $fetch_cart['quantity']); ?>/-</span> </div>
+            </form>
+            <?php
+                    $grand_total += $sub_total;
+                    }
+                }else{
+                    echo '<p class="empty">your cart is empty</p>';
+                }
+            ?>
+        </div>
+
+        <div class="more-btn">
+            <form action="" method="post">
+                <button type="submit" class="delete-btn <?= ($grand_total > 1)?'':'disabled'; ?>" name="delete_all" onclick="return confirm('delete all from cart?');">delete all</button>
+            </form>
+        </div>
+
+        <div class="cart-total">
+            <p>Total Price: <span>$<?= $grand_total; ?></span></p><br>
+            <a href="checkout.php" class="btn <?= ($grand_total > 1)?'':'disabled'; ?>">Checkout</a>
+        </div>
+
+        <div class="more-btn">
+            <a href="menu.php" class="continue-btn">< Continue Browsing</a>
+        </div>
+
+    </div>
+
+</body>
+
+<footer>
+    <br><br><?php include 'components/footer.php'; ?> 
+</footer>
+
+</html>
