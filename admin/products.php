c9a30cab8b43c18c48b8505b58415608f3eeaecf
Swapnil <46750394+swapsmax@users.noreply.github.com>
2022-10-31 16:53:45 +0800

    Updated all admin pages
    user_accounts.php not opening.
    Rest of the pages open as expected.

--- admin/products.php
+++ admin/products.php
@@ -1,3 +1,69 @@
+<?php
+
+include '../components/connect.php';
+
+session_start();
+
+$admin_id = $_SESSION['admin_id'];
+
+if(!isset($admin_id)){
+   header('location:admin_login.php');
+};
+
+if(isset($_POST['add_product'])){
+
+   $name = $_POST['name'];
+   $name = filter_var($name, FILTER_SANITIZE_STRING);
+   $price = $_POST['price'];
+   $price = filter_var($price, FILTER_SANITIZE_STRING);
+   $category = $_POST['category'];
+   $category = filter_var($category, FILTER_SANITIZE_STRING);
+
+   $image = $_FILES['image']['name'];
+   $image = filter_var($image, FILTER_SANITIZE_STRING);
+   $image_size = $_FILES['image']['size'];
+   $image_tmp_name = $_FILES['image']['tmp_name'];
+   $image_folder = '../uploaded_img/'.$image;
+
+   $select_products = $conn->prepare("SELECT * FROM `products` WHERE name = ?");
+   $select_products->execute([$name]);
+
+   if($select_products->rowCount() > 0){
+      $message[] = 'product name already exists!';
+   }else{
+      if($image_size > 2000000){
+         $message[] = 'image size is too large';
+      }else{
+         move_uploaded_file($image_tmp_name, $image_folder);
+
+         $insert_product = $conn->prepare("INSERT INTO `products`(name, category, price, image) VALUES(?,?,?,?)");
+         $insert_product->execute([$name, $category, $price, $image]);
+
+         $message[] = 'new product added!';
+      }
+
+   }
+
+}
+
+if(isset($_GET['delete'])){
+
+   $delete_id = $_GET['delete'];
+   $delete_product_image = $conn->prepare("SELECT * FROM `products` WHERE id = ?");
+   $delete_product_image->execute([$delete_id]);
+   $fetch_delete_image = $delete_product_image->fetch(PDO::FETCH_ASSOC);
+   unlink('../uploaded_img/'.$fetch_delete_image['image']);
+   $delete_product = $conn->prepare("DELETE FROM `products` WHERE id = ?");
+   $delete_product->execute([$delete_id]);
+   $delete_cart = $conn->prepare("DELETE FROM `cart` WHERE pid = ?");
+   $delete_cart->execute([$delete_id]);
+   header('location:products.php');
+
+}
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -23,14 +89,14 @@
 
    <form action="" method="POST" enctype="multipart/form-data">
       <h3>add product</h3>
-      <input type="text" required placeholder="enter product name" name="name" maxlength="100" class="box">
-      <input type="number" min="0" max="9999999999" required placeholder="enter product price" name="price" onkeypress="if(this.value.length == 10) return false;" class="box">
+      <input type="text" required placeholder="Enter Product Name" name="name" maxlength="100" class="box">
+      <input type="number" min="0" max="9999999999" required placeholder="Enter Product Price" name="price" onkeypress="if(this.value.length == 10) return false;" class="box">
       <select name="category" class="box" required>
-         <option value="" disabled selected>select category --</option>
-         <option value="main dish">main dish</option>
-         <option value="fast food">fast food</option>
-         <option value="drinks">drinks</option>
-         <option value="desserts">desserts</option>
+         <option value="" disabled selected>Select Category --</option>
+         <option value="main dish">Main Dish</option>
+         <option value="fast food">Fast Food</option>
+         <option value="drinks">Drinks</option>
+         <option value="desserts">Desserts</option>
       </select>
       <input type="file" name="image" class="box" accept="image/jpg, image/jpeg, image/png, image/webp" required>
       <input type="submit" value="add product" name="add_product" class="btn">
