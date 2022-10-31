1544569fc2cbc922c388d2ebcd120da86fe7cd71
Swapnil <46750394+swapsmax@users.noreply.github.com>
2022-10-31 23:31:52 +0800

    Updated contact.php
    Added contact.php
    Fixed login.php misalignment in form and texxt

--- login.php
+++ login.php
@@ -1,10 +1,44 @@
+<?php
+
+include 'components/connect.php';
+
+session_start();
+
+if(isset($_SESSION['user_id'])){
+   $user_id = $_SESSION['user_id'];
+}else{
+   $user_id = '';
+};
+
+if(isset($_POST['submit'])){
+
+   $email = $_POST['email'];
+   $email = filter_var($email, FILTER_SANITIZE_STRING);
+   $pass = sha1($_POST['pass']);
+   $pass = filter_var($pass, FILTER_SANITIZE_STRING);
+
+   $select_user = $conn->prepare("SELECT * FROM `users` WHERE email = ? AND password = ?");
+   $select_user->execute([$email, $pass]);
+   $row = $select_user->fetch(PDO::FETCH_ASSOC);
+
+   if($select_user->rowCount() > 0){
+      $_SESSION['user_id'] = $row['id'];
+      header('location:home.php');
+   }else{
+      $message[] = 'Incorrect username or password!';
+   }
+
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Login</title>
+   <title>login</title>
 
    <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
@@ -16,21 +50,20 @@
 <body>
    
 <!-- header section starts  -->
-   <?php include 'components/nav_header.php'; ?>
+<?php include 'components/nav_header.php'; ?>
 <!-- header section ends -->
 
-<div class="form-container">
+<section class="form-container">
 
    <form action="" method="post">
-      <h3>Login and order something delicious!</h3>
-      <input type="email" name="email" required placeholder="Email" class="box" maxlength="50" oninput="this.value = this.value.replace(/\s/g, '')">
-      <input type="password" name="pass" required placeholder="Password" class="box" maxlength="50" oninput="this.value = this.value.replace(/\s/g, '')">
-      <input type="submit" value="LOGIN" name="submit" class="btn">
-      
-      <p><br>Don't have an account? <br><a href="register.php" style="color: #4FAE5A; font-weight: bolder;">Register Now!</a></p>
+      <h3>Login Now</h3>
+      <input type="email" name="email" required placeholder="Enter your email" class="box" maxlength="50" oninput="this.value = this.value.replace(/\s/g, '')">
+      <input type="password" name="pass" required placeholder="Enter your password" class="box" maxlength="50" oninput="this.value = this.value.replace(/\s/g, '')">
+      <input type="submit" value="login now" name="submit" class="btn">
+      <p>Don't have an account? <a href="register.php" style="color: #50AD5A;">Register Now</a></p>
    </form>
 
-</div>
+</section>
 
 
 
@@ -42,7 +75,7 @@
 
 
 
-<?php include 'components/footer.php'; ?> 
+<?php include 'components/footer.php'; ?>
 
 
 
