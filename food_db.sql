3333c8ef3e280dce61aeba37b50fc0668e2f41ab
nemopotatoes <jesgjx@gmail.com>
2022-11-07 23:17:29 +0800

    updated database

--- food_db.sql
+++ food_db.sql
@@ -84,7 +84,7 @@ CREATE TABLE orders (
   FOREIGN KEY (user_id) REFERENCES users(id)
 );
 INSERT INTO orders (id, user_id, name, number, email, method, address, total_products, total_price, placed_on) VALUES (
-  1, 1, 'user1', '123', 'user1@email', 'paylah', 'address1', 'total_products', 16.80, '10/10/2022'
+  1, 1, 'user1', '123', 'user1@email.com', 'paylah', 'address1', 'total_products', 16.80, '10/10/2022'
 );
 
 CREATE TABLE messages (
