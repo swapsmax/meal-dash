acbb79b2d89cb7887a8cb3de2094d4307f6fbadb
nemopotatoes <jesgjx@gmail.com>
2022-11-08 01:56:08 +0800

    fixed - admin registration notification

--- admin/register_admin.php
+++ admin/register_admin.php
@@ -56,6 +56,20 @@ if(isset($_POST['submit'])){
 
 <?php include '../components/admin_header.php' ?>
 
+<!-- confirmation message -->
+<?php
+if(isset($message)){
+   foreach($message as $message){
+      echo '
+      <div class="message">
+         <span>'.$message.'</span>
+         <i class="fas fa-times" onclick="this.parentElement.remove();"></i>
+      </div>
+      ';
+   }
+}
+?>
+
 <!-- register admin section starts  -->
 
 <section class="form-container">
@@ -71,21 +85,6 @@ if(isset($_POST['submit'])){
 </section>
 
 <!-- register admin section ends -->
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 <!-- custom js file link  -->
 <script src="../js/admin_script.js"></script>
