32915edafccf07abc66e62e3b923aed78cda9f82
nemopotatoes <jesgjx@gmail.com>
2022-11-07 22:57:12 +0800

    updated users default account

--- food_db.sql
+++ food_db.sql
@@ -27,7 +27,7 @@ CREATE TABLE users (
 INSERT INTO users (id, name, email, number, password, address) VALUES (
   1,
   'user1',
-  'jessie.heihei@gmail',
+  'user1@gmail.com,
   '123',
   '123',
   '123'
