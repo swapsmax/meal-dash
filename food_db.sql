d24b60813afea84b0ff6d632499c7f55dbce7f52
nemopotatoes <jesgjx@gmail.com>
2022-11-09 03:39:40 +0800

    corrected food db file

--- food_db.sql
+++ food_db.sql
@@ -1,261 +1,82 @@
--- phpMyAdmin SQL Dump
--- version 5.2.0
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Nov 08, 2022 at 09:27 AM
--- Server version: 10.4.24-MariaDB
--- PHP Version: 8.1.6
+-- drop tables for initialising to avoid reference key error
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+DROP TABLE IF EXISTS admin;
+DROP TABLE IF EXISTS cart;
+DROP TABLE IF EXISTS orders;
+DROP TABLE IF EXISTS messages;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS products;
 
+CREATE TABLE admin (
+  id INT(100) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name varchar(20) NOT NULL,
+  password varchar(50) NOT NULL
+);
+INSERT INTO admin (id, name, password) VALUES (
+  1, 'admin', '123'
+);
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+CREATE TABLE users (
+  id INT(100) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name varchar(20) NOT NULL,
+  email varchar(50) NOT NULL,
+  number varchar(10) NOT NULL,
+  password varchar(50) NOT NULL,
+  address varchar(500) NOT NULL
+);
+INSERT INTO users (id, name, email, number, password, address) VALUES (
+  1,
+  'user1',
+  'user1@gmail.com',
+  '123',
+  '123',
+  '123'
+);
 
---
--- Database: `mealdash`
---
+CREATE TABLE products (
+  id INT(100) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name varchar(100) NOT NULL,
+  category varchar(100) NOT NULL,
+  price double(10,2) NOT NULL,
+  image varchar (100)
+);
+INSERT INTO products (id, name, category, price, image) VALUES (
+  1,  'Double Cheese Burger', 'mains',  7.50, 'burger-1.png'
+);
+CREATE TABLE cart (
+  id int(100) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  user_id int(100) UNSIGNED NOT NULL,
+  pid int(100) UNSIGNED NOT NULL,
+  name varchar(100) NOT NULL,
+  price double(10,2) NOT NULL,
+  quantity int(10) NOT NULL,
+  image varchar(100) NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (pid) REFERENCES products(id)
+);
 
--- --------------------------------------------------------
+CREATE TABLE orders (
+  id int(100) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  user_id int(100) UNSIGNED NOT NULL,
+  name varchar(20) NOT NULL,
+  number varchar(10) NOT NULL,
+  email varchar(50) NOT NULL,
+  method varchar(50) NOT NULL,
+  address varchar(500) NOT NULL,
+  total_products varchar(1000) NOT NULL,
+  total_price double(100,2) NOT NULL,
+  placed_on date NOT NULL DEFAULT current_timestamp(),
+  payment_status varchar(20) NOT NULL DEFAULT 'pending',
+  order_status varchar(50) NOT NULL DEFAULT 'order submitted',
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
 
---
--- Table structure for table `admin`
---
-
-CREATE TABLE `admin` (
-  `id` int(100) UNSIGNED NOT NULL,
-  `name` varchar(20) NOT NULL,
-  `password` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `admin`
---
-
-INSERT INTO `admin` (`id`, `name`, `password`) VALUES
-(1, 'admin', '123'),
-(2, 'admin2', '345');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `cart`
---
-
-CREATE TABLE `cart` (
-  `id` int(100) UNSIGNED NOT NULL,
-  `user_id` int(100) UNSIGNED NOT NULL,
-  `pid` int(100) UNSIGNED NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `price` double(10,2) NOT NULL,
-  `quantity` int(10) NOT NULL,
-  `image` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `messages`
---
-
-CREATE TABLE `messages` (
-  `id` int(100) UNSIGNED NOT NULL,
-  `user_id` int(100) UNSIGNED NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `number` varchar(12) NOT NULL,
-  `message` varchar(500) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `orders`
---
-
-CREATE TABLE `orders` (
-  `id` int(100) UNSIGNED NOT NULL,
-  `user_id` int(100) UNSIGNED NOT NULL,
-  `name` varchar(20) NOT NULL,
-  `number` varchar(10) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `method` varchar(50) NOT NULL,
-  `address` varchar(500) NOT NULL,
-  `total_products` varchar(1000) NOT NULL,
-  `total_price` double(100,2) NOT NULL,
-  `placed_on` date NOT NULL DEFAULT current_timestamp(),
-  `payment_status` varchar(20) NOT NULL DEFAULT 'pending',
-  `order_status` varchar(50) NOT NULL DEFAULT 'order submitted'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `orders`
---
-
-INSERT INTO `orders` (`id`, `user_id`, `name`, `number`, `email`, `method`, `address`, `total_products`, `total_price`, `placed_on`, `payment_status`, `order_status`) VALUES
-(5, 1, 'John Appleseed', '91234567', 'user1@gmail.com', 'credit card', '#01-02, 123 Nanyang Crescent, Singapore - 612345', 'Double Cheese Burger (7.5 x 2) - ', 15.00, '2022-11-08', 'completed', 'delivered'),
-(6, 1, 'John Appleseed', '91234567', 'user1@gmail.com', 'paypal', '#01-02, 123 Nanyang Crescent, Singapore - 612345', 'Iced Coffee (2 x 3) - Double Cheese Burger (7.5 x 3) - ', 28.50, '2022-11-08', 'completed', '');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `products`
---
-
-CREATE TABLE `products` (
-  `id` int(100) UNSIGNED NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `category` varchar(100) NOT NULL,
-  `price` double(10,2) NOT NULL,
-  `image` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `products`
---
-
-INSERT INTO `products` (`id`, `name`, `category`, `price`, `image`) VALUES
-(1, 'Double Cheese Burger', 'mains', 7.50, 'burger-1.png'),
-(3, 'Iced Coffee', 'drinks', 2.00, 'iced-coffee.jpeg'),
-(4, 'Nasi Lemak', 'main dish', 5.00, 'nasi-lemak.png'),
-(5, 'Banana Split', 'desserts', 6.00, 'banana-split.jpeg');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-CREATE TABLE `users` (
-  `id` int(100) UNSIGNED NOT NULL,
-  `name` varchar(20) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `number` varchar(10) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  `address` varchar(500) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `name`, `email`, `number`, `password`, `address`) VALUES
-(1, 'John Appleseed', 'user1@gmail.com', '91234567', '123', '#01-02, 123 Nanyang Crescent, Singapore - 612345'),
-(2, 'test', 'test@gmail.com', '123', '123', '');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `admin`
---
-ALTER TABLE `admin`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `cart`
---
-ALTER TABLE `cart`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `user_id` (`user_id`),
-  ADD KEY `pid` (`pid`);
-
---
--- Indexes for table `messages`
---
-ALTER TABLE `messages`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `user_id` (`user_id`);
-
---
--- Indexes for table `orders`
---
-ALTER TABLE `orders`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `user_id` (`user_id`);
-
---
--- Indexes for table `products`
---
-ALTER TABLE `products`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `admin`
---
-ALTER TABLE `admin`
-  MODIFY `id` int(100) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `cart`
---
-ALTER TABLE `cart`
-  MODIFY `id` int(100) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-
---
--- AUTO_INCREMENT for table `messages`
---
-ALTER TABLE `messages`
-  MODIFY `id` int(100) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `orders`
---
-ALTER TABLE `orders`
-  MODIFY `id` int(100) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
--- AUTO_INCREMENT for table `products`
---
-ALTER TABLE `products`
-  MODIFY `id` int(100) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(100) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `cart`
---
-ALTER TABLE `cart`
-  ADD CONSTRAINT `cart_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
-  ADD CONSTRAINT `cart_ibfk_2` FOREIGN KEY (`pid`) REFERENCES `products` (`id`);
-
---
--- Constraints for table `messages`
---
-ALTER TABLE `messages`
-  ADD CONSTRAINT `messages_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
-
---
--- Constraints for table `orders`
---
-ALTER TABLE `orders`
-  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+CREATE TABLE messages (
+  id int(100) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  user_id int(100) UNSIGNED NOT NULL,
+  name varchar(100) NOT NULL,
+  email varchar(100) NOT NULL,
+  number varchar(12) NOT NULL,
+  message varchar(500) NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
